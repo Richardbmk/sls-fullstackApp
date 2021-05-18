663c9a7f30a9e811d1e7fb36e57316e3c8186a52
Ricardo <rdobmk@gmail.com>
2021-05-18 22:25:26 +0200

    auth implemented

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment }  from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+
 export default class ProductAdmin extends Component {
 
   state = {
